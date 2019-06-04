673830840b50f5a92969ab4fba47b720e8f9cf6b
a0920069082b <a0920069082b@gmail.com>
2019-06-04 21:46:19 +0800

    【身份】建立Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Database\Seeder\Base\ConfigTableSeeder;
+use Database\Seeder\Base\IdentityTableSeeder;
 use Database\Seeder\Base\PermissionTableSeeder;
 use Database\Seeder\Base\RoleTableSeeder;
 use Database\Seeder\Base\UserTableSeeder;
@@ -17,10 +18,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(PermissionTableSeeder::class);
         $this->call(RoleTableSeeder::class);
+        $this->call(IdentityTableSeeder::class);
         $this->call(UserTableSeeder::class);
         $this->call(ConfigTableSeeder::class);
-
-        if (config('app.env') === 'local') {
-        }
+        if (config('app.env') === 'local') { }
     }
 }
---
+++ database/seeds/base/IdentityTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeder\Base;
+
+use App\Model\Eloquent\Identity;
+use Illuminate\Database\Seeder;
+
+class IdentityTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $arr = ['老師', '研究生', 'Lab 1.0', 'Lab 2.0', 'Lab 3.0', 'Lab 4.0', 'Lab 5.0', 'Lab 6.0', 'Lab 7.0', 'Lab 8.0', 'Lab 9.0'];
+        for ($i = 0; $i < count($arr); $i++) {
+            $this->createIdentityGroup($arr[$i]);
+        }
+    }
+
+    public function createIdentityGroup($identity)
+    {
+        Identity: create([
+            'identities' => $identity
+        ]);
+    }
+}
